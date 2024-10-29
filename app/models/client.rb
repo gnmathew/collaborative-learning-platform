95ef0185080d95a72861e0c3f72a6d4f21613255
gnmathew <genemathewdorado@gmail.com>
2024-10-29 15:08:06 +0800

    [ Add ] Feature Generate ID number for client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,23 @@
 class Client < ApplicationRecord
   has_secure_password
+  after_validation :generate_client_id_number
 
   enum role: { student: 0, teacher: 1 }
   enum status: { active: 0, inactive: 1 }
 
-  validates :email, :username, presence: true
+  validates :email, :username, presence: true, uniqueness: true
+
+  private
+
+  def generate_client_id_number
+    user_role = student? ? 'ST' : 'IN'
+    self.id_number = "#{user_role}#{Time.current.year.to_s}#{set_user_id}"
+  end
+
+  def set_user_id
+    start_year = Time.current.beginning_of_year
+    end_year = Time.current.end_of_year
+    user_id = Client.where(role: role, created_at: start_year..end_year)
+    (user_id.count + 1).to_s.rjust(3, '0')
+  end
 end
