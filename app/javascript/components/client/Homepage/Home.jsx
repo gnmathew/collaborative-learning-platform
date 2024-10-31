c0a5c8d3f99bb66c47059821143c2bb1d16f774b
gnmathew <genemathewdorado@gmail.com>
2024-10-31 15:16:53 +0800

    [ Add ] Client Home page

---
+++ app/javascript/components/client/Homepage/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import MainMenu from "./MainMenu"
+import styled, { keyframes } from 'styled-components';
+
+const zoomIn = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+const Wrapper = styled.div`
+  animation: ${zoomIn} 1.5s ease-in-out;
+`;
+
+const Home = () => {
+  return(
+    <div>
+      <div>
+        <Wrapper>
+          <MainMenu />
+        </Wrapper>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
