bb2f0e8bd22a71055e37cb46676e48a144f13eba
gnmathew <genemathewdorado@gmail.com>
2024-11-27 14:55:00 +0800

    [ Fix ] scroll enabled after modal is dismissed

--- app/javascript/utils/modalUtils.js
+++ app/javascript/utils/modalUtils.js
@@ -1,11 +1,19 @@
 export const closeModal = (id = null) => {
-  const modalElement = id ? document.getElementById(`exampleModal-${id}`) : document.getElementById('exampleModal')
+  const modalElement = id
+    ? document.getElementById(`exampleModal-${id}`)
+    : document.getElementById('exampleModal');
 
   if (modalElement) {
     modalElement.classList.remove('show');
     modalElement.style.display = 'none';
+    modalElement.setAttribute('aria-hidden', 'true');
+    modalElement.removeAttribute('aria-modal');
+    modalElement.removeAttribute('role');
+
     document.body.classList.remove('modal-open');
+    document.body.style.overflow = '';
     document.body.style.paddingRight = '';
+
     const backdrop = document.querySelector('.modal-backdrop');
     if (backdrop) backdrop.remove();
   }
